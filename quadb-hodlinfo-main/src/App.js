9520dcb153f0ac6399002faf1ad3d6e760789603
Akshay Deshmukh <deshmukhaksh@outlook.com>
2021-09-09 17:09:48 +0530

    second commit

--- quadb-hodlinfo-main/src/App.js
+++ quadb-hodlinfo-main/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Footer } from '../src/components/Footer'
 import {Header } from '../src/components/Header'
+import {App } from '../src/components/App.html'
 import {MainPage } from '../src/components/MainPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/components/styles.css';
